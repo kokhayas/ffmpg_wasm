c737fcb6b0702274daaa17b0e7c803f3748b76ed
KokiHayashi <1234.884.koki@gmail.com>
2022-04-29 21:02:36 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import React, { useEffect, useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {createFFmpeg, fetchFile} from '@ffmpeg/ffmpeg';
 const ffmpeg = createFFmpeg({
 	corePath: "/ffmpeg_core_dist/ffmpeg-core.js",
 	log: true
@@ -11,10 +11,14 @@ function App() {
 	const [ready, setReady] = useState(false);
 	const [video, setVideo] = useState();
 	const [gif, setGif] = useState();
+	const [width, setWidth] = useState(250);
+	const [height, setHeight] = useState(200);
+	const [startTime, setStartTime] = useState(2.0);
+	const [endTime, setEndTime] = useState(2.5);
 
 	const convertToGIf = async () => {
 		ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
-		await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2', '-f', 'gif', 'out.gif');
+		await ffmpeg.run('-i', 'test.mp4', '-t', `${endTime}`, '-vf', `scale=${width}:${height}`, '-ss', `${startTime}`, '-f', 'gif', 'out.gif');
 		const data = ffmpeg.FS('readFile', 'out.gif');
 		const url = URL.createObjectURL(new Blob([data.buffer], {type: 'image/gif'}));
 		setGif(url);
@@ -33,10 +37,17 @@ function App() {
   return ready ? (
     <div className="App">
 		{video && <video controls width="250"src={URL.createObjectURL(video)}></video>}
+		<br></br>
 		<input type="file" onChange={(e) => setVideo(e.target.files?.item(0))}/>
+<div>
+		width: <input type="text" value={width} onChange={(e) => setWidth(e.target.value)} />px
+		height: <input type="text" value={height} onChange={(e) => setHeight(e.target.value)}/>px <br></br>
+		start time: <input type="text" value={startTime} onChange={(e) => setStartTime(e.target.value)} />s
+		end time: <input type="text" value={endTime} onChange={(e) => setEndTime(e.target.value)} />s
+</div>
 		<h3>Result</h3>
 		<button onClick={convertToGIf}>Convert</button>
-		{ gif && <img src={gif} width="250"/>}
+		{ gif && <img src={gif} width={width}/>}
     </div>
   ) :
 	(<p>Loading...</p>);

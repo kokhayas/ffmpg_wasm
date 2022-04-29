26e4b20c6b053604c119698749a56a4c5f92d4a2
KokiHayashi <1234.884.koki@gmail.com>
2022-04-29 19:21:50 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {createFFmpeg, fetchFile} from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({
+	corePath: "/ffmpeg_core_dist/ffmpeg-core.js",
+	log: true
+});
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
+	const [ready, setReady] = useState(false);
+	const [video, setVideo] = useState();
+	const [gif, setGif] = useState();
+
+	const convertToGIf = async () => {
+		ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+		await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2', '-f', 'gif', 'out.gif');
+		const data = ffmpeg.FS('readFile', 'out.gif');
+		const url = URL.createObjectURL(new Blob([data.buffer], {type: 'image/gif'}));
+		setGif(url);
+	}
+
+	const load = async () => {
+		await ffmpeg.load();
+		setReady(true);
+		console.log("hello");
+	};
+
+	useEffect(() => {
+		load();
+	}, [])
+
+  return ready ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+		{video && <video controls width="250"src={URL.createObjectURL(video)}></video>}
+		<input type="file" onChange={(e) => setVideo(e.target.files?.item(0))}/>
+		<h3>Result</h3>
+		<button onClick={convertToGIf}>Convert</button>
+		{ gif && <img src={gif} width="250"/>}
     </div>
-  );
+  ) :
+	(<p>Loading...</p>);
 }
 
 export default App;
--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,5 +1,14 @@
 process.env.NODE_ENV = 'test';
 
+
 module.exports = {
   plugins: [require('@snowpack/web-test-runner-plugin')()],
+};
+
+module.exports = function (app) {
+    app.use(function (request, response, next) {
+        response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+        response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+        next();
+    });
 };
